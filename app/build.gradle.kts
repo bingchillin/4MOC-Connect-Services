75b57d1f6f8cb7b0a9f430a2fad10a40b6bd1f73
bingchillin <92374981+bingchillin@users.noreply.github.com>
2024-07-01 17:06:37 +0200

    Fixed gradle issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.material3.android)
 
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
